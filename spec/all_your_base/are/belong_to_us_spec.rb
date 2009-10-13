e88fc41322f4cafe60df1c6c9460f5e8615f8a67
Shane <shanewolf@gmail.com>
2009-10-13 15:58:31 -0700

    fix require for spec_helper

--- spec/all_your_base/are/belong_to_us_spec.rb
+++ spec/all_your_base/are/belong_to_us_spec.rb
@@ -1,4 +1,4 @@
-require File.expand_path(File.dirname(__FILE__) + '/../spec_helper')
+require File.expand_path(File.dirname(__FILE__) + '/../../spec_helper')
 require 'all_your_base/are/belong_to_us'
 
 describe AllYourBase::Are::BelongToUs::From do
