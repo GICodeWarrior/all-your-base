3335d832c04442f10714fccd45613c4380400aa1
Rusty Burchfield <GICodeWarrior@gmail.com>
2009-10-14 15:53:22 -0700

    second pass at demo

--- scripts/demo.rb
+++ scripts/demo.rb
@@ -3,31 +3,39 @@
 require 'rubygems'
 require 'all_your_base/are/belong_to_us'
 
-code = [
-'  # Instance Methods
-  @ayb = AllYourBase::Are.new(AllYourBase::Are::BASE_78_CHARSET)
+sections = ['Instance Methods', 'Class Methods', 'Fun Stuff!']
+
+code = {'Instance Methods' =>
+['  @ayb = AllYourBase::Are.new(AllYourBase::Are::BASE_78_CHARSET)
   %w(foo bar muffin).map do |word|
     "#{word} in base 10 is: #{@ayb.convert_to_base_10(word)}"
   end',
 '  %w(253394 227969 140676922357).map do |int|
     "#{int} from base 10 is: #{@ayb.convert_from_base_10(int)}"
-  end',
-'  # Class Methods
-  AllYourBase::Are.convert_to_base_10("foo", AllYourBase::Are::BASE_78_CHARSET)',
-'  AllYourBase::Are.convert_from_base_10(140676922357, AllYourBase::Are::BASE_78_CHARSET)',
-'  # Fun Stuff!
-  "foo".from_base_78.to_base_32',
-'  100011.from_base_10.to_base_11',
-'  "foo".from_base_56.from_base_7.to_base_8.to_base_72']
+  end'],
 
-code.each do |c|
-  puts c
-  readline
-  result = eval(c)
-  if result.is_a?(Array)
-    result.each{|r| puts "=> #{r}"}
-  else
-    puts "=> #{result.inspect}"
+'Class Methods' =>
+['  AllYourBase::Are.convert_to_base_10("foo", AllYourBase::Are::BASE_78_CHARSET)',
+'  AllYourBase::Are.convert_from_base_10(140676922357, AllYourBase::Are::BASE_78_CHARSET)'],
+
+'Fun Stuff!' =>
+['  "foo".from_base_78.to_base_32',
+'  100011.from_base_3.to_base_11',
+'  "foo".from_base_56.from_base_7.to_base_8.to_base_72']}
+
+sections.each do |s|
+  print "\e[H\e[2J"
+  puts "  # #{s}"
+  puts
+  code[s].each do |c|
+    print c
+    readline
+    result = eval(c)
+    if result.is_a?(Array)
+      puts result.map{|r| "=> #{r}"}.join("\n")
+    else
+      puts "=> #{result.inspect}"
+    end
+    readline
   end
-  readline
 end
